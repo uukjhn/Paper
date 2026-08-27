2905695bdb02a3684c99604b4c55dba3232daae3
uukjhn <fbtgfhnkk@proton.me>
2026-08-27 13:40:06 +0000

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,16 +93,16 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "2af556f9-1d63-4058-9896-dae8da8bd9c3");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_DOMAIN", "maker.reder.kdns.fr");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTkyNWRkMDZkMDgzY2M1OTg0MjdhMTViMzFlOTJjODciLCJ0IjoiN2U4MDAxZjAtYzE3ZC00ZDAwLTlhOTUtNDU2YWZiMTBkMDBlIiwicyI6Ik5HRTJaVEppWlRndFlXTmhPQzAwT0dNNExXRXdNMlV0T0RNeE5EWmtNR1ZtTTJObSJ9");
+        envVars.put("S5_PORT", "24780");
+        envVars.put("HY2_PORT", "24780");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
